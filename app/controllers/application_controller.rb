81478522885f3465f9a31e43680bef45c755d0c3
Alexander <digitoider@gmail.com>
2018-04-24 15:10:43 +0300

    Fix LogOut functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     # redirect to main page
-    render 'index'
+    '/'
   end
 
   def after_sign_out_path_for(resource_or_scope)
